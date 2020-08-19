f8db3e7f035337b68cb41a3ca9783e52bfbff633
james2kim <54189655+james2kim@users.noreply.github.com>
2020-08-19 05:51:14 -0700

    Completed Leap Year Algorithm

--- Completed-Algorithms/Leap_Year/Leap_Year.js
+++ Completed-Algorithms/Leap_Year/Leap_Year.js
@@ -1,4 +1,8 @@
 function isLeapYear(year) {
+//   If the year is divisible by 400, it is a leap year
+  //  if the year is divisible by 100, the it is not a leap year
+//   If the year is divisible by 4, then it is a leap year, as long as it is not divisble by 100 
+//   in any other situation, it is not a leap year
   if (year % 400 === 0) {
     return true
   } else if (year % 100 === 0) {
