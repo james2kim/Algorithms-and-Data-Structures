6322243c56836177902cb278aef2e9184d99859b
james2kim <54189655+james2kim@users.noreply.github.com>
2020-09-08 00:29:35 -0700

    Completed Standard Deviation Algorithm

--- Completed-Algorithms/Standard_Deviation/Standard_Deviation.js
+++ Completed-Algorithms/Standard_Deviation/Standard_Deviation.js
@@ -2,10 +2,11 @@ function standardDeviation(arr) {
   let sum = 0
   let sdSum = 0
   let varianceArray = []
+  let average, variance
 // Compute the Mathematical Average 
   for (let i = 0;i < arr.length; i++) {
     sum += arr[i]
-    let average = sum/arr.length
+    average = sum/arr.length
   }
   // Inside of the Variance Array, put the values of the dataset subtract the mathematical mean 
   for(let i = 0;i < arr.length; i++) {
@@ -14,7 +15,7 @@ function standardDeviation(arr) {
 // To Calculate variance, we need to take the sum of the new values divided by number of values
   for (let i = 0;i < varianceArray.length; i++){
     sdSum += varianceArray[i]
-    let variance = sdSum / varianceArray.length
+    variance = sdSum / varianceArray.length
   }
 // To calculate SD, square root the variance 
   const standardDeviation = Math.sqrt(variance)
