5c6daff15e6265fd5e64f4d79c7e3fccc51417ec
james2kim <54189655+james2kim@users.noreply.github.com>
2020-09-08 01:21:14 -0700

    Completed Bubble Sort Algorithm

--- Completed-Algorithms/Bubble_Sort/BubbleSort.js
+++ Completed-Algorithms/Bubble_Sort/BubbleSort.js
@@ -1,29 +1,21 @@
 
 function bubbleSort (array) {
-
-  let swapC=1;
-  let swapCC=0;
-
-  while(swapC > 0) {
-
-    swapCC=0;
-
-    for (let i = 0, n = array.length; i < n; i++) {
-
-        if (array[i] > array[i+1]) {
-          const temp = array[i];
-          array[i] = array[i+1];
-          array[i+1] = temp;
-          swapCC++;
+  let swapC = 1;
+  let swapCC = 0;
+  
+    while(swapC > 0) {
+      swapCC = 0;
+      for (let i = 0, n = array.length; i < n; i++) {
+         if (array[i] > array[i+1]) {
+            const temp = array[i];
+            array[i] = array[i+1];
+            array[i+1] = temp;
+            swapCC++;
         }
-
     }
-
     swapC = swapCC;
-
   }
   return array
-
 }
 
 bubbleSort([6,5,4,3,2,1])
