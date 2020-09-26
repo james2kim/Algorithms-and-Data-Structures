274ad16699c60ae6fd7661fdc40518dcf529f2a6
James Kim <pgcjameskim@gmail.com>
2020-09-25 18:03:35 -0700

    Create Largest_Numbers_Arrays.js

---
+++ Completed-Algorithms/Basic_Algorithms/Largest_Numbers_Arrays/Largest_Numbers_Arrays.js
@@ -0,0 +1,10 @@
+function largestOfFour(arr) {
+  let newArray = []
+  for (let i = 0; i < arr.length; i++) {
+    const max = arr[i].reduce((a,b) => Math.max(a,b))
+    newArray.push(max)
+  }
+  return newArray
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
