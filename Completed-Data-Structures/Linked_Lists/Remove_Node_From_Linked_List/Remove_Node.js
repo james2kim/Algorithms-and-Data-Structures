98d5f2bbc5437d4667621bba0fb32429bd7fea88
James Kim <pgcjameskim@gmail.com>
2020-11-18 21:42:00 -0800

    Create Remove_Node.js

---
+++ Completed-Data-Structures/Linked_Lists/Remove_Node_From_Linked_List/Remove_Node.js
@@ -0,0 +1,56 @@
+function LinkedList() {
+  let length = 0;
+  let head = null;
+
+  const Node = function(element){
+    this.element = element;
+    this.next = null;
+  };
+
+  this.size = function(){
+    return length;
+  };
+
+  this.head = function(){
+    return head;
+  };
+
+  this.add = function(element){
+    const node = new Node(element);
+    if(head === null){
+        head = node;
+    } else {
+      var currentNode = head;
+
+      while(currentNode.next){
+        currentNode  = currentNode.next;
+      }
+
+      currentNode.next = node;
+    }
+
+    length++;
+  };
+
+  this.remove = function(element){
+    // Only change code below this line
+  if (head.element === element) {
+    head = head.next
+    return length --
+  }
+  let previous = head
+      while (previous !== null) {
+      let current = previous.next;
+      if (current) {
+        if (current.element === element) {
+          previous.next = current.next;
+          return length--;
+        }
+      }
+      previous = current;
+    }
+
+
+    // Only change code above this line
+  };
+}
