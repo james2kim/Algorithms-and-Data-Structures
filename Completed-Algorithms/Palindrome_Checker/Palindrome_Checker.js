4c45f74286fb389d725912ec71adbaa5a3931ecd
James Kim <pgcjameskim@gmail.com>
2020-12-24 13:10:06 -0800

    Update Palindrome_Checker.js

--- Completed-Algorithms/Palindrome_Checker/Palindrome_Checker.js
+++ Completed-Algorithms/Palindrome_Checker/Palindrome_Checker.js
@@ -25,17 +25,10 @@ palindrome("_eye")
 function palindrome(str) {
 //First, for ease if comparison, we will lowercase the string and remove all white spaces and special characters 
 const filteredString = str.replace(/[^0-9a-z]/gi, '').toLowerCase()
-
 // Reverse the string 
 const reversedString = filteredString .split('').reverse().join('')
-
-// Compare reversed string to the filtered string 
-  if(reversedString === filteredString) {
-    return true;
-  } else {
-    return false; 
-  }
-
+// Compare reversed string to the filtered string  
+ return reversedString === filteredString ? true : false 
 }
 
 
