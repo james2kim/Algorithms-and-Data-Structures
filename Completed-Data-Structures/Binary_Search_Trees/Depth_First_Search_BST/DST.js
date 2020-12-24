069f46aa221e0f57eef816df60bd5c8a22be0be3
James Kim <pgcjameskim@gmail.com>
2020-12-24 15:42:14 -0800

    Create DST.js

---
+++ Completed-Data-Structures/Binary_Search_Trees/Depth_First_Search_BST/DST.js
@@ -0,0 +1,46 @@
+var displayTree = tree => console.log(JSON.stringify(tree, null, 2));
+function Node(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+function BinarySearchTree() {
+  this.root = null;
+  // Only change code below this line
+  this.inorder = function() {
+    if (!this.root) return null
+    let visited = []
+    function traverse (node) {
+      if (node.left) traverse(node.left)
+      visited.push(node.value)
+      if (node.right) traverse(node.right)
+    }
+    traverse(this.root)
+    return visited
+  }
+
+  this.preorder = function() {
+    if (!this.root) return null
+    let visited  = []
+    function traverse(node) {
+      visited.push(node.value)
+      if(node.left) traverse(node.left)
+      if(node.right) traverse(node.right)
+    }
+    traverse(this.root)
+    return visited
+  }
+
+  this.postorder = function() {
+    if (!this.root) return null
+    let visited = []
+    function traverse(node) {
+      if (node.left) traverse(node.left)
+      if(node.right) traverse(node.right)
+      visited.push(node.value)
+    }
+    traverse(this.root)
+    return visited
+  }
+  // Only change code above this line
+}
