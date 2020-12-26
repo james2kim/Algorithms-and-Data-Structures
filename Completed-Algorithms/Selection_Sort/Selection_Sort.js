fc0c1e781ea0ceb2b36e736d19a2b7e93d8cac32
James Kim <pgcjameskim@gmail.com>
2020-12-26 01:04:07 -0800

    Update Selection_Sort.js

--- Completed-Algorithms/Selection_Sort/Selection_Sort.js
+++ Completed-Algorithms/Selection_Sort/Selection_Sort.js
@@ -1,16 +1,15 @@
-function selectionSort(array){
-  const length = array.length
-  let innerIndex = 0
-  for (let i = 0; i < length; i++) {
-    for (let j = innerIndex; j < array.length; j++) {
-      if (array [j] < array [i]) {
-       const temp = array[i];
-       array[i] = array[j];
-       array[j] = temp;
-     }
+function selectionSort(array) {
+    let index = 0
+
+    for (let i = 0; i < array.length; i++) {
+        let lowest = i
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[j] < array[lowest]) {
+                lowest = j 
+            }
+        }
+            [array[i], array[lowest]] = [array[lowest], array[i]]
     }
-    innerIndex++
-  }
-  return array
+    return array
 }
-selectionSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92])
+selectionSort([6,5,4,3,2,1])
