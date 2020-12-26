327ad9ecc920bde3ef6768280867508854bed8ac
James Kim <pgcjameskim@gmail.com>
2020-12-25 21:52:32 -0800

    Create AdjacencyMatrix.js

---
+++ Completed-Data-Structures/Graphs/Adjacency_Matrix/AdjacencyMatrix.js
@@ -0,0 +1,17 @@
+const adjMatUndirected = [
+    [0,0,1,1,0],
+    [0,0,0,0,0],
+    [1,0,0,0,1],
+    [1,0,0,0,1],
+    [0,0,1,1,0]
+];
+
+// undirectedAdjList should only contain five nodes.
+
+// There should be an edge between the first and fourth node.
+
+// There should be an edge between the first and third node.
+
+// There should be an edge between the third and fifth node.
+
+// There should be an edge between the fourth and fifth node.
