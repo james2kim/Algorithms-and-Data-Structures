c6bf867a953fbeeaa47a286f5e1ecbf1e0ab999c
James Kim <pgcjameskim@gmail.com>
2021-01-05 15:24:07 -0800

    Update Factorial.js

--- Completed-Algorithms/Basic_Algorithms/Factorial/Factorial.js
+++ Completed-Algorithms/Basic_Algorithms/Factorial/Factorial.js
@@ -1,12 +1,6 @@
-//Recursive Solution
 function factorialize(num) {
-  if (num === 0) {
-    return 1
-  } else if (num ===1) {
-    return num
-  } else {
-    return num * factorialize(num-1)
-  }
+  if (num === 0) return 1
+  return num * factorialize(num -1)
 }
 
-factorialize(5);
+console.log(factorialize(5))
