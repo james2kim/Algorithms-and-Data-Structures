0467d8d87bf96cb32c093fdf9b3b73c5ce668ee7
James Kim <pgcjameskim@gmail.com>
2021-01-26 12:39:41 -0800

    Update Binary_Search.js

--- Completed-Algorithms/Binary_Search/Binary_Search.js
+++ Completed-Algorithms/Binary_Search/Binary_Search.js
@@ -1,18 +1,15 @@
-function binarySearch (sortedArray, value) {
-    let start= 0
-    let end = sortedArray.length - 1
+function binarySearch(arr, value) {
+    let start = 0
+    let end = arr.length - 1
     let middle = Math.floor((start + end) / 2)
-
-    while(sortedArray[middle] !== value && start <= end) {
-        if (value < sortedArray[middle]) {
-            end = middle -1
-        } else {
-            start = middle + 1
-        }
+    
+    while (arr[middle] !== value && start <= end) {
+        if (value < arr[middle]) end = middle - 1
+        else start = middle + 1
         middle = Math.floor((start + end) / 2)
     }
-    if (sortedArray[middle] === value) {
-        return middle
-    } 
-        return - 1
+    return arr[middle] === value ? middle : -1
 }
+
+
+binarySearch([1,2,3,4,5,6,7], 1)
