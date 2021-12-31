02e6436ed38d6e433aa30345ad0980e45721cbb9
James Kim <pgcjameskim@gmail.com>
2021-12-30 16:35:14 -0800

    Update Cash_Register.js

--- Completed-Algorithms/Cash_Register/Cash_Register.js
+++ Completed-Algorithms/Cash_Register/Cash_Register.js
@@ -1,178 +1,48 @@
 function checkCashRegister(price, cash, cid) {
-  let descendingCID = [...cid].reverse()
-  let cidCopy = []
-
-  for (let i =0 ;i < cid.length; i++) {
-    cidCopy[i] = [...cid[i]]
+  let changeRequired = (cash - price).toFixed(2);
+  const currencyDictionary = {
+    "ONE HUNDRED": 100,
+    "TWENTY": 20,
+    "TEN": 10,
+    "FIVE": 5,
+    "ONE": 1,
+    "QUARTER": 0.25,
+    "DIME": 0.1,
+    "NICKEL": 0.5,
+    "PENNY": 0.01,
   }
-  const cashRegisterObject = {
-    status: '',
-    change: []
-  }
-
-  let changeNeeded = cash-price
-
-    let i = 0
-    let value = 0
-    let finalArray = []
-    let change = ['', 0]
-    while (changeNeeded >= 100 && descendingCID[i][1] > 0) {
-      change[0] = 'ONE HUNDRED'
-      changeNeeded -= 100
-      descendingCID[i][1] -=100
-      change[1] += 100
-      changeNeeded = Math.round(changeNeeded * 100) /100
-       change[1] = Math.round(change[1] * 100) /100
-      descendingCID[i][1] = Math.round(descendingCID[i][1] * 100) /100
-
-    }
-
-    if (change[0] !== ''){
-      finalArray.push(change)
-      change = ['', 0]
-    } 
-    i++
-
-    while (changeNeeded >= 20 && descendingCID[i][1] > 0) {
-      change[0] = 'TWENTY'
-      changeNeeded -= 20
-      descendingCID[i][1] -=20
-      change[1] +=20
-      changeNeeded = Math.round(changeNeeded * 100) /100
-       change[1] = Math.round(change[1] * 100) /100
-      descendingCID[i][1] = Math.round(descendingCID[i][1] * 100) /100
-    }
-    if (change[0] !== ''){
-      finalArray.push(change)
-      change = ['', 0]
-    } 
-    i++
-
-    while (changeNeeded >= 10 && descendingCID[i][1] > 0) {
-      change[0] = 'TEN'
-      changeNeeded -= 10
-      descendingCID[i][1] -=10
-      change[1] +=10
-      changeNeeded = Math.round(changeNeeded * 100) /100
-       change[1] = Math.round(change[1] * 100) /100
-      descendingCID[i][1] = Math.round(descendingCID[i][1] * 100) /100
-    }
-
-    if (change[0] !== ''){
-      finalArray.push(change)
-      change = ['', 0]
-    } 
-    i++
-
-    while (changeNeeded >= 5 && descendingCID[i][1] > 0) {
-      change[0] = 'FIVE'
-      changeNeeded -= 5
-      descendingCID[i][1] -= 5
-      change[1] += 5
-      changeNeeded = Math.round(changeNeeded * 100) /100
-       change[1] = Math.round(change[1] * 100) /100
-      descendingCID[i][1] = Math.round(descendingCID[i][1] * 100) /100
-    }
-    if (change[0] !== ''){
-      finalArray.push(change)
-      change = ['', 0]
-    } 
-    i++
-
-    while (changeNeeded >= 1 && descendingCID[i][1] > 0) {
-      change[0] = 'ONE'
-      changeNeeded -= 1
-      descendingCID[i][1] -= 1
-      change[1] += 1
-      changeNeeded = Math.round(changeNeeded * 100) /100
-       change[1] = Math.round(change[1] * 100) /100
-      descendingCID[i][1] = Math.round(descendingCID[i][1] * 100) /100
-    }
-    if (change[0] !== ''){
-      finalArray.push(change)
-      change = ['', 0]
-    } 
-    i++
-
-    while (changeNeeded >= 0.25 && descendingCID[i][1] > 0) {
-      change[0] = 'QUARTER'
-      changeNeeded -= 0.25
-      descendingCID[i][1] -= 0.25
-      change[1] += 0.25
-      changeNeeded = Math.round(changeNeeded * 100) /100
-       change[1] = Math.round(change[1] * 100) /100
-      descendingCID[i][1] = Math.round(descendingCID[i][1] * 100) /100
-  
-    }
-    if (change[0] !== ''){
-      finalArray.push(change)
-      change = ['', 0]
-    } 
-    i++
-
-    while (changeNeeded >= 0.10 && descendingCID[i][1] > 0) {
-      change[0] = 'DIME'
-      changeNeeded -= 0.10
-      descendingCID[i][1] -= 0.10
-      change[1] += 0.10
-      changeNeeded = Math.round(changeNeeded * 100) /100
-       change[1] = Math.round(change[1] * 100) /100
-      descendingCID[i][1] = Math.round(descendingCID[i][1] * 100) /100
-    }
-    if (change[0] !== ''){
-      finalArray.push(change)
-      change = ['', 0]
-    } 
-    i++
-
-    while (changeNeeded >= 0.05 && descendingCID[i][1] > 0) {
-      change[0] = 'NICKEL'
-      changeNeeded -= 0.05
-      descendingCID[i][1] -= 0.05
-      change[1] += 0.05
-      changeNeeded = Math.round(changeNeeded * 100) /100
-      change[1] = Math.round(change[1] * 100) /100
-      descendingCID[i][1] = Math.round(descendingCID[i][1] * 100) /100
-    }
-    if (change[0] !== ''){
-      finalArray.push(change)
-      change = ['', 0]
-    } 
-    i++
-
-    while (changeNeeded >= 0.01 && descendingCID[i][1] > 0) {
-      change[0] = 'PENNY'
-      changeNeeded -= 0.01
-     change[1] += 0.01
-      descendingCID[i][1] -= 0.01
-      changeNeeded = Math.round(changeNeeded * 100) /100
-       change[1] = Math.round(change[1] * 100) /100
-      descendingCID[i][1] = Math.round(descendingCID[i][1] * 100) /100
-      
-      console.log(change[1])
-    }
-    if (change[0] !== ''){
-
-      finalArray.push(change)
-      change = ['', 0]
-    } 
-  let availableCash = 0
-  for (let i = 0; i < descendingCID.length; i++) {
-  
-   availableCash += descendingCID[i][1]
-  }
-  console.log(finalArray)
-  if (changeNeeded > 0) {
-    cashRegisterObject.status = 'INSUFFICIENT_FUNDS'
-  } else if (finalArray.length > 0 && availableCash === 0) {
-     cashRegisterObject.change = cidCopy
-     cashRegisterObject.status = 'CLOSED'
+  const change = [];
+  const cidSorted = [...cid].reverse()
+  const amountInRegister = cid.map((item) => item[1]).reduce((a,b) => a + b)
+  if (amountInRegister < changeRequired) {
+    return {status: "INSUFFICIENT_FUNDS", change: []}
+  } else if (amountInRegister.toFixed(2) === changeRequired) {
+    return {status: "CLOSED", change: cid}
   } else {
-    cashRegisterObject.change = finalArray
-     cashRegisterObject.status = 'OPEN'
+    cidSorted.forEach((curID) => {
+    let currencyAmount
+    if (currencyDictionary[curID[0]] <= changeRequired && curID[1] > 0) {
+        currencyAmount = [curID[0], 0]
+    }
+
+    while (currencyDictionary[curID[0]] <= changeRequired && curID[1] > 0) {
+      currencyAmount[1] += currencyDictionary[curID[0]]
+      curID[1] -= currencyDictionary[curID[0]]
+      changeRequired -= currencyDictionary[curID[0]]
+      changeRequired = changeRequired.toFixed(2)
+          if (changeRequired === 0) {
+            break;
+          }
+    }
+    if (currencyAmount) {
+      change.push(currencyAmount)
+    }
+  })
+  if (changeRequired > 0) {
+    return {status: "INSUFFICIENT_FUNDS", change: []}
   }
-    return cashRegisterObject
-   
+    return {status: "OPEN", change}
+  }
 }
 
 checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
