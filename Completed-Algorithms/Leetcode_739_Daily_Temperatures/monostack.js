7b4278dfc7b1efed19871f267e35e0aa6cae4066
James Kim <pgcjameskim@gmail.com>
2025-03-18 20:45:30 -0700

    Update monostack.js

--- Completed-Algorithms/Leetcode_739_Daily_Temperatures/monostack.js
+++ Completed-Algorithms/Leetcode_739_Daily_Temperatures/monostack.js
@@ -5,16 +5,14 @@
 var dailyTemperatures = function(temperatures) {
     const stack = []
     const answers = new Array(temperatures.length).fill(0)
+
     for(let i = 0; i < temperatures.length; i++) {
-        while(stack.length > 0  && temperatures[i] > stack[stack.length - 1][1]) {
-            const prevTemp = stack[stack.length - 1][1]
-            const prevIndex = stack[stack.length - 1][0]
-            if(temperatures[i] > prevTemp) {
-                answers[prevIndex] = i - prevIndex
-                stack.pop()
-            }
+        while(stack.length > 0 && temperatures[i] > stack[stack.length - 1].temp) {
+            const {prevIndex} = stack[stack.length - 1]
+            answers[prevIndex] = i - prevIndex
+            stack.pop()
         }
-        stack.push([i, temperatures[i]])
+        stack.push({prevIndex: i, temp: temperatures[i]})
     }
     return answers
 };
