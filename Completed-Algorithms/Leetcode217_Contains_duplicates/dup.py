2dd79f8b56c59e2dc6d4c46ce9de15416e433b8a
James Kim <pgcjameskim@gmail.com>
2026-03-30 15:11:54 -0700

    Update dup.py

--- Completed-Algorithms/Leetcode217_Contains_duplicates/dup.py
+++ Completed-Algorithms/Leetcode217_Contains_duplicates/dup.py
@@ -1,10 +1,20 @@
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if(len(s) != len(t)): 
+            return False
         dictionary = {}
-        for num in nums:
-            if num in dictionary:
-                return True
-            dictionary[num] = True
-        return False
-        
-        
+        for char in s: 
+            if char not in dictionary:
+                dictionary[char] = 1
+            else:
+               dictionary[char] += 1
+        for char in t:
+            if char not in dictionary:
+                return False
+            dictionary[char] -= 1
+
+        for v in dictionary.values():
+            if v != 0:
+                return False
+        return True
+
