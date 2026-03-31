41df8ca4d1aa33dbcd362ba07a5c6e584aa5bbc9
James Kim <pgcjameskim@gmail.com>
2026-03-31 10:11:45 -0700

    Create anagram.py

---
+++ Completed-Algorithms/Leetcode_242_ValidAnagram/anagram.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if(len(s) != len(t)): 
+            return False
+        dictionary = {}
+        for char in s: 
+            if char not in dictionary:
+                dictionary[char] = 1
+            else:
+               dictionary[char] += 1
+        for char in t:
+            if char not in dictionary:
+                return False
+            dictionary[char] -= 1
+
+        for v in dictionary.values():
+            if v != 0:
+                return False
+        return True
