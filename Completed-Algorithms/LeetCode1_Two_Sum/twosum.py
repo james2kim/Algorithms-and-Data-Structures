3f2d8614c53afb35ea0dd2f74e97bc018878b805
James Kim <pgcjameskim@gmail.com>
2026-03-31 10:53:46 -0700

    Update twosum.py

--- Completed-Algorithms/LeetCode1_Two_Sum/twosum.py
+++ Completed-Algorithms/LeetCode1_Two_Sum/twosum.py
@@ -1,15 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dictionary = {}
-        result = []
         for i, num in enumerate(nums):
-            if i!=0 and target - num in dictionary.keys():
-                for k,v in dictionary.items():
-                    if target - k == num:
-                        result.extend([v, i])
-                        break
-
-            if not num in dictionary.keys():
+            if target - num in dictionary:
+                return[dictionary[target - num], i]
+            if not num in dictionary:
                 dictionary[num] = i
+    
+
         
-        return result
