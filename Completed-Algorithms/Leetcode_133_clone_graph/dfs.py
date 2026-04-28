6061644fda8782504b265754c19fb00bdc18ec23
James Kim <pgcjameskim@gmail.com>
2026-04-28 13:01:23 -0700

    Create dfs.py

---
+++ Completed-Algorithms/Leetcode_133_clone_graph/dfs.py
@@ -0,0 +1,23 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        visited = {}
+        def dfs(node):
+            if not node:
+                return None
+            if node in visited:
+                return visited[node]
+            copy = Node(node.val, [])
+            visited[node] = copy
+            for neighbor in node.neighbors:
+                copy.neighbors.append(dfs(neighbor))
+            return copy
+        return dfs(node)
