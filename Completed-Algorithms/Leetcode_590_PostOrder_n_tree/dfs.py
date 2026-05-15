262455501d009db3adc42c92e069297711a14330
James Kim <pgcjameskim@gmail.com>
2026-05-15 14:59:54 -0700

    Create dfs.py

---
+++ Completed-Algorithms/Leetcode_590_PostOrder_n_tree/dfs.py
@@ -0,0 +1,20 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        result = []
+        def dfs(node):
+            if not node:
+                return
+            for c in node.children:
+                dfs(c)
+            result.append(node.val)
+        dfs(root)
+        return result
+        
